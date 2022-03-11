ce241067d82904f1b3c343629ee1961cebcb4353
Mehmet Can Aydın <mehmet1can1aydin@gmail.com>
2022-03-11 22:41:34 +0300

    Uploading Files

---
+++ Kuantum Sayıları/Kaynak Kodlar/Kuantum Sayılarına Göre Katman Sayısını Bulma/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Kuantum_Sayılarına_Göre_Katman_Sayısını_Bulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "s"});
+            this.comboBox1.Location = new System.Drawing.Point(596, 157);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 33);
+            this.comboBox1.TabIndex = 0;
+             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(8, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 133);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1) Baş Kuantum Sayısı:\r\n(Proje raporuna uygun olması için 8s \'e kadar geçerlidir." +
+    ")";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(379, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 88);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2) Açısal Momentum (İkincil) Kuantum Sayısını Seçiniz:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(210, 267);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(115, 29);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tam Dolu";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(331, 267);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(162, 29);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Boş Orbital Var";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(195, 342);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 33);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(13, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 106);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "3) Doluluk Oranı:\r\nBoş Orbital Varsa Son Katmandaki Elektron Sayısı:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(272, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 88);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Başlarındaki Sayılara Göre Sırayla Bilgileri Doldurunuz.";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numericUpDown1.Location = new System.Drawing.Point(223, 157);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 30);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(405, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 54);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(357, 407);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(218, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Toplam Elektron Sayısı:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Ana Sayfa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Kuantum Sayıları/Kaynak Kodlar/Kuantum Sayılarına Göre Katman Sayısını Bulma/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kuantum_Sayılarına_Göre_Katman_Sayısını_Bulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Programlayan Mehmet Can AYDIN
+
+        void Combobox1Values(int kirma)
+        {
+            //daha kısa kodla halletmek için döngü kullanmayı tercih ettim.
+            comboBox1.Items.Clear();
+            comboBox1.Text = "";
+
+            while (true)
+            {
+                comboBox1.Items.Add("s");
+                if (kirma == 0) break;
+
+                comboBox1.Items.Add("p");
+                if (kirma == 1) break;
+
+                comboBox1.Items.Add("d");
+                if (kirma == 2) break;
+
+                comboBox1.Items.Add("f");
+                break;
+            }
+        }
+
+        void ComboBox2Values(int kirma)
+        {
+            //daha kısa kodla halletmek için döngü kullanmayı tercih ettim.
+            comboBox2.Items.Clear();
+            comboBox2.Text = "";
+
+            for (int i = 0; i < kirma; i++)
+            {
+                comboBox2.Items.Add(i);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.Enabled = false;
+            radioButton1.Checked = true;
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            // Doluluk durumuna göre son orbitaldeki elektron sayısı ayarlanıyor.
+            comboBox2.Enabled = radioButton2.Checked;
+
+            // Açısal Momentum Kuantum Sayısına Göre Orbitalde olabilecek elektron sayısını ekleme
+
+            comboBox2.Items.Clear();
+
+            switch (comboBox1.Text)
+            {
+                case "s":
+                    ComboBox2Values(2); break;
+                case "p":
+                    ComboBox2Values(6); break;
+                case "d":
+                    ComboBox2Values(10); break;
+                case "f":
+                    ComboBox2Values(14); break;
+                default:
+                    ComboBox2Values(0); break;
+            }
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Baş Kuantum Sayısına Göre İkincil Kuantum Sayısını Ayarlamamızı Sağlayan Kutucuğu Ayarlama
+
+            switch (numericUpDown1.Value)
+            {
+                case 1:
+                    Combobox1Values(0); break;
+                case 2:
+                    Combobox1Values(1); break;
+                case 3:
+                    Combobox1Values(2); break;
+                default:
+                    Combobox1Values(3); break;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text == "")
+            {
+                MessageBox.Show("Açısal Momentum Kuantum Sayısının Değeri Boş Bırakılmamalıdır!","Hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else if(radioButton2.Checked && comboBox2.Text == "")
+            {
+                MessageBox.Show("Son Katmanda Kaç Elektron Olduğunun Bilgisi Boş Bırakılmamalıdır!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                /*buradan irtibaren hesaplamalar başladığı için
+                 * proje raporunda formüllere verilen değerler kullanılmıştır */
+
+                int lverilen, n = Convert.ToInt32(numericUpDown1.Value);
+                int sonuc = 0;
+
+                switch (comboBox1.Text)
+                {
+                    case "s":
+                        lverilen = 0; break;
+                    case "p":
+                        lverilen = 1; break;
+                    case "d":
+                        lverilen = 2; break;
+                    default:
+                        lverilen = 3; break;
+                }
+
+                for (int listenen = 0; listenen < 5; listenen++)
+                {
+                    int ofazla = lverilen - listenen;
+
+                    int orbital = n - listenen + ofazla;
+
+                    if (ofazla > 0) orbital--; //"--"   -1'i ifade ediyor.
+                    if (orbital <= 0) break;
+
+                    switch (listenen)
+                    {
+                        case 0:
+                            sonuc += orbital * 2; break;
+                        case 1:
+                            sonuc += orbital * 6; break;
+                        case 2:
+                            sonuc += orbital * 10; break;
+                        default:
+                            sonuc += orbital * 14; break;
+                    }
+                }
+
+                //son katmandaki eksik elektrona göre kontrol etme
+                if (radioButton2.Checked)
+                {
+                    int son_katman = Convert.ToInt32(comboBox2.Text);
+                    int normalde_olan;
+
+                    switch (comboBox1.Text)
+                    {
+                        case "s":
+                            normalde_olan = 2; break;
+                        case "p":
+                            normalde_olan = 6; break;
+                        case "d":
+                            normalde_olan = 10; break;
+                        default:
+                            normalde_olan = 14; break;
+                    }
+
+                    sonuc -= normalde_olan - son_katman;
+                }
+
+                //sonucu son label'a yazdırmak.
+                label5.Text = "Toplam Elektron Sayısı: " + sonuc;
+            }
+        }
+    }
+}
